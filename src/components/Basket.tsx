bdb7a7b8b1322f40c84bf0093f79b540291db896
EcSid <166507541+EcSid@users.noreply.github.com>
2024-07-12 14:37:52 +0300

    Update Basket.tsx to phone users

--- src/components/Basket.tsx
+++ src/components/Basket.tsx
@@ -26,7 +26,7 @@ const Basket: FC = () => {
     <>
         <div className='allBoughtSneakers' style={{textAlign: 'center'}}>
             <h1 style={{
-                marginTop: window.innerWidth < 735 ? '12%' : '8%',
+                marginTop: window.innerWidth < 735 ? '13%' : '8%',
                 marginBottom: '-0.5%',
             }}>Корзина</h1>
             {elemArr.map((id, index) => {
