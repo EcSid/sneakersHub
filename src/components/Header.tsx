b6a66423236900d8d0901268d2f85e6aecda5030
EcSid <166507541+EcSid@users.noreply.github.com>
2024-07-12 13:46:29 +0300

    add absolute path to sneakers logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -40,7 +40,7 @@ const Header: FC = () => {
                 
             }}>
                 <div style={{display: 'flex', alignItems: 'center'}}>
-                    <img src='../image.png' width='38px' height='38px' style={{marginRight: '10px'}}/>
+                    <img src='https://avatars.mds.yandex.net/i?id=830b3895ddeb656a7b47d90f850fc8bf334dde65-8497209-images-thumbs&n=13' width='38px' height='38px' style={{marginRight: '10px'}}/>
                     <h2 tabIndex={1} style={{ color: 'black'}}>Sneakers Hub</h2>
                 </div>
                 <div className='icons' style={{display: 'flex'}}>
