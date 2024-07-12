7ec15aef55fd21228de0fe3c636556f1a1ca9153
EcSid <166507541+EcSid@users.noreply.github.com>
2024-07-12 18:49:35 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,6 @@ import Header from "./components/Header";
 
 
 const App = () => {
-  if (window.innerWidth < 767) {
-    document.documentElement.style = {
-        overflow-x: hidden;
-        overflow-y: scroll;
-        min-height: 2100px;
-        max-height: 2100px
-    }
-  
-    document.body.style = {
-        overflow-x: hidden;
-        overflow-y: scroll;
-        min-height: 2100px;
-        max-height: 2100px
-    }   
-  }
-
-  }
   return (
     <>
        <Header /> 
